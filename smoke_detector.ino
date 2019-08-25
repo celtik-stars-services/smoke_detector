d8f99cbb6e01c078a617a252897372df866c3c5e
peb <pebouchard@gmail.com>
2019-08-25 03:25:29 -0400

    Added wifi. Will connect. No Mqtt send

--- smoke_detector.ino
+++ smoke_detector.ino
@@ -1,19 +1,37 @@
 #include <MQ2.h>
-#include <Wire.h> 
+#include <Wire.h> //?
+#include <ESP8266WiFi.h>
 
 int Analog_Input = A0;
 int lpg, co, smoke;
 
 MQ2 mq2(Analog_Input);
 
+// Update these with values suitable for your network.
+const char* ssid = "dlink-5060";
+const char* password = "ge06nity90";
+const char* mqtt_server = "192.168.0.186";
+
 void setup(){
   Serial.begin(9600);
 
   mq2.begin();
+  WiFi.begin("ssid", "password");
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+  
 }
 void loop(){
-  float* values= mq2.read(true);
-
+  float* values= mq2.read(true); 
   lpg = mq2.readLPG();
 
   co = mq2.readCO();
@@ -30,5 +48,4 @@ void loop(){
   Serial.print(" PPM");
   delay(1000);
 }
-
 
